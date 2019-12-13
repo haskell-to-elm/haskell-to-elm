de23c8b81d65845d31f8855d174bb416b06461f1
Olle Fredriksson <fredriksson.olle@gmail.com>
2019-12-13 15:51:59 +0100

    Add example

---
+++ examples/User.hs
@@ -0,0 +1,54 @@
+{-# LANGUAGE DeriveAnyClass #-}
+{-# LANGUAGE DeriveGeneric #-}
+{-# LANGUAGE MultiParamTypeClasses #-}
+{-# LANGUAGE NoImplicitPrelude #-}
+{-# LANGUAGE OverloadedStrings #-}
+{-# LANGUAGE TypeApplications #-}
+module Simple where
+
+import Protolude
+
+import qualified Data.Aeson as Aeson
+import Data.Time
+import qualified Data.HashMap.Lazy as HashMap
+import qualified Generics.SOP as SOP
+
+import qualified Language.Elm.Pretty as Pretty
+import Language.Haskell.To.Elm
+
+data User = User
+  { name :: Text
+  , birthday :: Day
+  } deriving
+    ( Generic
+    , Aeson.ToJSON
+    , SOP.Generic
+    , SOP.HasDatatypeInfo
+    , HasElmDecoder Aeson.Value
+    , HasElmEncoder Aeson.Value
+    , HasElmType
+    )
+
+instance HasElmDefinition User where
+  elmDefinition =
+    deriveElmTypeDefinition @User defaultOptions "Api.User.User"
+
+instance HasElmDecoderDefinition Aeson.Value User where
+  elmDecoderDefinition =
+    deriveElmJSONDecoder @User defaultOptions Aeson.defaultOptions "Api.User.decoder"
+
+instance HasElmEncoderDefinition Aeson.Value User where
+  elmEncoderDefinition =
+    deriveElmJSONEncoder @User defaultOptions Aeson.defaultOptions "Api.User.encoder"
+
+main :: IO ()
+main = do
+  let
+    definitions =
+      jsonDefinitions @User
+
+    modules =
+      Pretty.modules definitions
+
+  forM_ (HashMap.toList modules) $ \(_moduleName, contents) ->
+    print contents
